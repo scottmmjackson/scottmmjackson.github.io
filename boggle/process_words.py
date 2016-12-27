b7f408e7f71cf8bfb57d34feac25d8e94f7f9388
Scott Jackson <scottmmjackson@gmail.com>
2016-12-27 16:23:26 -0500

    fixed small bug in trie generator

--- boggle/process_words.py
+++ boggle/process_words.py
@@ -10,6 +10,6 @@ words = (word.lower().rstrip() for word in inp.readlines() if (len(word) >= 3 an
 def group_recursive(m_words):
   if not m_words:
    return {"!": True}
-  return {k:group_recursive([j[1:] for j in g if j[1:]]) for k,g in groupby(m_words,key=lambda word: word[0])}
+  return {k:group_recursive([j[1:] if j[1:] else '' for j in g]) if k != '!' else True for k,g in groupby(m_words,key=lambda word: word[0] if word else '!')}
 
 print "var DICT = {}".format(dumps(group_recursive(words)))
