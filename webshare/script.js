46694fb3b7c6e9388bb60383f537632c4ea39e85
Scott Jackson <scottmmjackson@gmail.com>
2022-02-14 16:26:24 -0500

    add webshare demo

---
+++ webshare/script.js
@@ -0,0 +1,11 @@
+var shareData = document.getElementById('share-data');
+var shareTarget = document.getElementById('share-target');
+
+shareTarget.addEventListener('click', function(ev) {
+	if (typeof navigator.share !== 'function') {
+		console.error('navigator.share not detected');
+		return;
+	}
+	var shareDataValue = shareData.value;
+	navigator.share(shareDataValue);
+});
