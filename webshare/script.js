3160d288f6542ab488d05c479dd4a4d79c25ab96
Scott Jackson <scottmmjackson@gmail.com>
2022-02-24 10:45:48 -0500

    update sharing demo

--- webshare/script.js
+++ webshare/script.js
@@ -1,18 +1,60 @@
+var shareTitle = document.getElementById('share-title');
 var shareData = document.getElementById('share-data');
 var shareTarget = document.getElementById('share-target');
 var messages = document.getElementById('messages');
+var shareFile = document.getElementById('share-file');
+var shareFileTarget = document.getElementById('share-file-target');
+var shareUrl = document.getElementById('share-url');
+var shareUrlTarget = document.getElementById('share-url-target');
 
 shareTarget.addEventListener('click', function(ev) {
 	if (typeof navigator.share !== 'function') {
-		console.error('navigator.share not detected');
+		messages.innerText = 'navigator.share not detected!'
 		return;
 	}
 	var shareDataValue = shareData.value;
-	var f = new File([shareDataValue], "data.txt", {type: "text/plain"})
+	var data = {
+		text: shareDataValue
+	}
+	if (shareTitle.value) {
+		data.title = shareTitle.value
+	}
+	navigator.share(data)
+		.then(res => messages.innerText = 'Share Successful!')
+		.catch(err => messages.innerText = 'Share Unsuccessful! ' + err.toString());
+});
 
-	navigator.share({
-		files: [f]
-	})
-		.then(res => messages.text = 'Share Successful! '+ res.toString())
-		.catch(err => messages.text = 'Share Unsuccessful! ' + err.toString());
+shareFileTarget.addEventListener('click', function(ev) {
+	if (typeof navigator.share !== 'function') {
+		messages.innerText = 'navigator.share not detected!'
+		return;
+	}
+	if (shareFile.files.length === 0) {
+		messages.innerText = 'no files added!'
+	}
+	var data = {
+		files: shareFile.files
+	}
+	if (shareTitle.value) {
+		data.title = shareTitle.value
+	}
+	navigator.share(data)
+		.then(res => messages.innerText = 'Share Successful!')
+		.catch(err => messages.innerText = 'Share Unsuccessful! ' + err.toString());
+});
+
+shareUrlTarget.addEventListener('click', function(ev) {
+	if (typeof navigator.share !== 'function') {
+		messages.innerText = 'navigator.share not detected!'
+		return;
+	}
+	var data = {
+		url: shareUrl.value,
+	}
+	if (shareTitle.value) {
+		data.title = shareTitle.value
+	}
+	navigator.share(data)
+		.then(res => messages.innerText = 'Share Successful!')
+		.catch(err => messages.innerText = 'Share Unsuccessful! ' + err.toString());
 });
